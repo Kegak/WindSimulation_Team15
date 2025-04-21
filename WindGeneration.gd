de21cd36bc04c83e7dca2acacc0ff98061095889
Kegak <keeganjsims@gmail.com>
2025-04-21 17:09:12 -0500

    Created new scene

--- WindGeneration.gd
+++ WindGeneration.gd
@@ -1,6 +1,7 @@
 class_name WindGeneration extends Fluid2D
 @export var fluid: Fluid2D
 @export var objects: Node2D
+@export var y: int
 
 
 var points_array: PackedVector2Array
@@ -12,7 +13,7 @@ var dir : Vector2
 
 func _ready() -> void:
 
-	points_array = create_rectangle_points(1,2) # X means rows and Y is Collumns
+	points_array = create_rectangle_points(1,y) # X means rows and Y is Collumns
 
 	added_vector.resize(points_array.size())
 	var gravity_value = ProjectSettings.get("physics/2d/default_gravity")
@@ -29,7 +30,7 @@ func _process(delta: float) -> void:
 		add_points_and_velocities(points_array,added_vector)
 	var points = fluid.points
 	var velocities = fluid.get_velocities()
-	fluid.set_points_and_velocities(points, velocities)
+	#fluid.set_points_and_velocities(points, velocities)
 	var all_objects = objects.get_children()
 	for object in all_objects:
 		var collsion_zone = object.get_child(0)
@@ -41,7 +42,7 @@ func _process(delta: float) -> void:
 				for point in points:
 					if ((object.get_global_transform()[2][0] - (width/2) <= (fluid.get_global_transform()[2][0] + point.x)) and ((fluid.get_global_transform()[2][0] + point.x) <= object.get_global_transform()[2][0] + (width/2))) and ((object.get_global_transform()[2][1] - (height/2) <= (fluid.get_global_transform()[2][1] + point.y)) and ((fluid.get_global_transform()[2][1] + point.y) <= object.get_global_transform()[2][1] + (height/2))):
 						velocities.set(index, collsion())
-						set_points_and_velocities(fluid.points, velocities)
+						set_points_and_velocities(points, velocities)
 					index += 1
 					
 			if (collsion_zone.get_shape() is CircleShape2D):
@@ -49,7 +50,7 @@ func _process(delta: float) -> void:
 				for point in points:
 					if (((fluid.get_global_transform()[2][0] + point.x) - object.get_global_transform()[2][0])**2 + ((fluid.get_global_transform()[2][1] + point.y) - object.get_global_transform()[2][1])**2) < radius**2:
 						velocities.set(index, collsion())
-						set_points_and_velocities(fluid.points, velocities)
+						set_points_and_velocities(points, velocities)
 					index += 1
 		
 	
