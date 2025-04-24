52aa875a5a80f0ee16995f31978d1f64b72a1d6b
Kegak <keeganjsims@gmail.com>
2025-04-24 13:43:47 -0500

    Fixes

--- WindGeneration.gd
+++ WindGeneration.gd
@@ -1,19 +1,16 @@
 class_name WindGeneration extends Fluid2D
 @export var fluid: Fluid2D
 @export var objects: Node2D
-
+@export var y : int
 
 var points_array: PackedVector2Array
 var added_vector: PackedVector2Array
-var timer : int = 10
-var x : int = 0
+var timer : int = 0
 var grav_dir_opposite : Vector2 
 var dir : Vector2
 
 func _ready() -> void:
-
-	points_array = create_rectangle_points(1,2) # X means rows and Y is Collumns
-
+	points_array = create_rectangle_points(1,y)
 	added_vector.resize(points_array.size())
 	var gravity_value = ProjectSettings.get("physics/2d/default_gravity")
 	var gravity_dir = ProjectSettings.get("physics/2d/default_gravity_vector")
@@ -24,12 +21,10 @@ func _ready() -> void:
 	
 
 func _process(delta: float) -> void:
-
-	if x % 5 == 0:
+	if timer % 5 == 0:
 		add_points_and_velocities(points_array,added_vector)
 	var points = fluid.points
-	var velocities = fluid.get_velocities()
-	fluid.set_points_and_velocities(points, velocities)
+	var velocities = fluid.get_velocities().duplicate()
 	var all_objects = objects.get_children()
 	for object in all_objects:
 		var collsion_zone = object.get_child(0)
@@ -40,7 +35,7 @@ func _process(delta: float) -> void:
 				var width = collsion_zone.shape.size.x
 				for point in points:
 					if ((object.get_global_transform()[2][0] - (width/2) <= (fluid.get_global_transform()[2][0] + point.x)) and ((fluid.get_global_transform()[2][0] + point.x) <= object.get_global_transform()[2][0] + (width/2))) and ((object.get_global_transform()[2][1] - (height/2) <= (fluid.get_global_transform()[2][1] + point.y)) and ((fluid.get_global_transform()[2][1] + point.y) <= object.get_global_transform()[2][1] + (height/2))):
-						velocities.set(index, collsion())
+						velocities.set(index, boltzmann())
 						set_points_and_velocities(fluid.points, velocities)
 					index += 1
 					
@@ -48,30 +43,32 @@ func _process(delta: float) -> void:
 				var radius = collsion_zone.shape.radius
 				for point in points:
 					if (((fluid.get_global_transform()[2][0] + point.x) - object.get_global_transform()[2][0])**2 + ((fluid.get_global_transform()[2][1] + point.y) - object.get_global_transform()[2][1])**2) < radius**2:
-						velocities.set(index, collsion())
+						velocities.set(index, boltzmann())
 						set_points_and_velocities(fluid.points, velocities)
 					index += 1
-		
+	timer += 1
 	
-func collsion() -> Vector2:
+func boltzmann() -> Vector2:
+	'''Implementation of Lattice Boltzmann equations giving random directions to fluid points as collision is detected'''
 	var rng = RandomNumberGenerator.new()
 	var my_random_number = rng.randi_range(1, 36)
-	if (my_random_number >= 1) and (my_random_number <= 16):
+	if (my_random_number <= 16):
 		return Vector2(0,0) 
-	if (my_random_number >= 17) and (my_random_number <= 20):
+	elif (my_random_number <= 20):
 		return Vector2(0,1) 
-	if (my_random_number == 21):
+	elif (my_random_number == 21):
 		return Vector2(1,1) 
-	if (my_random_number >= 22) and (my_random_number <= 25):
+	elif (my_random_number <= 25):
 		return Vector2(1,0) 
-	if (my_random_number == 26):
+	elif (my_random_number == 26):
 		return Vector2(1,-1)
-	if (my_random_number >= 27) and (my_random_number <= 30):
+	elif (my_random_number <= 30):
 		return Vector2(0,-1)  
-	if (my_random_number == 31):
+	elif (my_random_number == 31):
 		return Vector2(-1,-1) 
-	if (my_random_number >= 32) and (my_random_number <= 35):
+	elif (my_random_number <= 35):
 		return Vector2(-1,0) 
-	if (my_random_number == 36):
+	elif (my_random_number == 36):
 		return Vector2(-1,1) 
-	return Vector2(0,0)
+	else:
+		return Vector2(0,0)
