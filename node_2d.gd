cda6304531cad5f9fdeeb93ddc60b3180af635c5
Jacob-Severson <jseverson@unomaha.edu>
2025-04-29 20:19:07 -0500

    Refining of the main menu

--- node_2d.gd
+++ node_2d.gd
@@ -11,25 +11,35 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_button_pressed() -> void:
-	#jacob room
+func _on_button_jacob_room_pressed() -> void:
 	get_tree().change_scene_to_file("res://RoomJacob.tscn")
 	#pass # Replace with function body.
 
 
 func _on_button_keegan_room_pressed() -> void:
-	#Keegan
 	get_tree().change_scene_to_file("res://big_room_with_window.tscn")
 	#pass # Replace with function body.
 
 
 func _on_button_james_room_pressed() -> void:
-	#James
 	get_tree().change_scene_to_file("res://node_2d_james.tscn")
 	#pass # Replace with function body.
 
 
-func _on_button_teagan_room_pressed() -> void:
-	#Teagan
+func _on_button_alt_room_pressed() -> void:
 	get_tree().change_scene_to_file("res://node_2d_alt.tscn")
+	#pass # Replace with function body.
+
+
+func _on_button_teagan_room_pressed() -> void:
+	get_tree().change_scene_to_file("res://ZimmerMitTur.tscn")
+	#pass # Replace with function body.
+
+func _on_button_base_room_pressed() -> void:
+	get_tree().change_scene_to_file("res://node_2d.tscn")
+	#pass # Replace with function body.
+
+#I think i configured the button wrong for this one or set up all the other buttons for the main menu in the wrong script.
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://MainMenu.tscn")
 	#pass # Replace with function body.
