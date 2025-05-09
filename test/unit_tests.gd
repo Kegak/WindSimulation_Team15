7a9cad7699da2719f6aec50f48525be715cdf6a6
Kegak <keeganjsims@gmail.com>
2025-05-08 19:51:32 -0500

    Updated the test with Teagan's changes

--- test/unit_tests.gd
+++ test/unit_tests.gd
@@ -16,3 +16,13 @@ func test_initial_spawn():
 func test_boltzmann_return():
 	var result = wind_generator.boltzmann()
 	assert_true(result is Vector2, "boltzmann should have return a Vector2")
+	
+##Check wind speed greater than zero
+func test_wind_speed_is_greater_than_zero():
+	assert_gt(wind_generator.wind_speed, 0, "Wind was set to either zero or less than zero, wind needs speed.")
+	
+
+## checks to see if there is more than one row of wind, which is known to cause issues in some rooms
+func test_is_there_more_than_one_wind_row():
+	assert_gt(wind_generator.rows_of_wind, 1, "Amount of wind is too low.")
+	
